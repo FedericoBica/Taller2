760d8e8f9c69b92815ac335adf8813a382daade4
Federico <fedebicasua@gmail.com>
2026-02-19 15:16:02 -0300

    Primer commit

---
+++ src/logica/vo/VOVenta.java
@@ -0,0 +1,31 @@
+package logica.vo;
+
+public class VOVenta {
+	 private int num;
+	 private String fecha;
+	 private String direccionEntrega;
+	 private String estado;
+	 private float montoTotal;	
+
+	 public VOVenta() {}
+	 
+	 public int getNum() {
+		 return num;
+	 }
+	 
+	 public String getFecha() {
+		 return fecha;
+	 }
+
+	 public String getDireccionEntrega() {
+	     return direccionEntrega;
+	 }
+
+	 public String getEstado() {
+	     return estado;
+	 }
+
+	 public float getMontoTotal() {
+	     return montoTotal;
+	 }
+}
