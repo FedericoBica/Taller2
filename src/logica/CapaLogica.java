410c2369374c85f24a3b050721bc3ecba14723df
GiuliBelt <giulianabeltran21@gmail.com>
2026-02-27 02:50:55 -0300

    Update CapaLogica.java

--- src/logica/CapaLogica.java
+++ src/logica/CapaLogica.java
@@ -132,7 +132,7 @@ public void comenzarVenta(LocalDate fecha, String direccion) throws FechaInvalid
 }
 
 
-public VORecaudacion totalVentasPostreEnFecha(String codigoPostre, LocalDate fecha)
+/*public VORecaudacion totalVentasPostreEnFecha(String codigoPostre, LocalDate fecha)
         throws PostreNoExisteException {
 
     if (!abbPostres.containsKey(codigoPostre)) {
@@ -194,4 +194,238 @@ public void guardarDatos() {
 }
 
 }
+*/
 
+//REQUERIMIENTO: 5
+public void agregarPostreAVenta(String codigo, int cantidad, int numeroVenta)
+        throws VentaNoExisteException, VentaYaFinalizadaException,
+               PostreNoExisteException, CantidadInvalidaException {
+ Venta ventaEncontrada = null;
+
+  int i=0;
+while (i < listaVentas.size() && ventaEncontrada == null) {
+      Venta v = listaVentas.get(i);
+       if (v.getNumero() == numeroVenta) {
+        ventaEncontrada = v;
+    }
+
+    i++;
+}
+if (ventaEncontrada == null) {
+        throw new VentaNoExisteException("No existe la venta con el numero: " + numeroVenta);
+  }
+//Accedo y verifico si esta en proceso
+    if (!ventaEncontrada.getEstado().equals("EN_PROCESO")) {
+        throw new VentaYaFinalizadaException("venta finalizada.");
+    }
+
+// Verifico postre
+    if (!abbPostres.containsKey(codigo)) {
+        throw new PostreNoExisteException("El postre no está registrado.");
+    }
+  Postre postre = abbPostres.get(codigo);
+
+  //Obtengo la orden a traves de la venta
+  Orden ordenVenta = ventaEncontrada.getOrden();
+
+  // Verificar límite 40
+    int cantidadActual = ordenVenta.calcularCantTotal();
+
+  if (cantidadActual + cantidad > 40) {
+        throw new CantidadInvalidaException("No se puede superar 40 unidades por venta.");
+    }
+  
+  ItemOrden existente = ordenVenta.find(codigo);
+    if (existente != null) {
+        int nuevaCantidad = existente.getCantidad() + cantidad;
+        existente.setCantidad(nuevaCantidad);
+
+} else {
+      ItemOrden nuevo = new ItemOrden(cantidad, postre);
+    ordenVenta.insBack(nuevo);
+}
+}
+
+// REQUERIMIENTO 6
+public void eliminarPostreDeVenta(String codigo, int cantidad, int numeroVenta)
+        throws VentaNoExisteException, VentaYaFinalizadaException,
+               PostreNoExisteException, CantidadInvalidaException {
+
+    // Busco la venta
+    Venta ventaEncontrada = null;
+    int i = 0;
+
+    while (i < listaVentas.size() && ventaEncontrada == null) {
+
+        Venta v = listaVentas.get(i);
+
+        if (v.getNumero() == numeroVenta) {
+            ventaEncontrada = v;
+        }
+
+        i++;
+    }
+
+    if (ventaEncontrada == null) {
+        throw new VentaNoExisteException("El numero de venta no existe.");
+    }
+
+    //Verifico estado
+    if (!ventaEncontrada.getEstado().equals("EN_PROCESO")) {
+        throw new VentaYaFinalizadaException("La orden ya fue pagada.");
+    }
+
+    //Busco el postre en la orden
+    Orden ordenVenta = ventaEncontrada.getOrden();
+    ItemOrden existente = ordenVenta.find(codigo);
+
+    if (existente == null) {
+        throw new PostreNoExisteException("El postre no existe en esta orden.");
+    }
+
+    if (cantidad > existente.getCantidad()) {
+        throw new CantidadInvalidaException("Cantidad invalida.");
+    }
+
+    int nuevaCantidad = existente.getCantidad() - cantidad;
+    existente.setCantidad(nuevaCantidad);
+
+    //Si quedó en 0, eliminar el item completo
+    if (nuevaCantidad == 0) {
+        ordenVenta.deleteOrden(codigo);
+    }
+
+    //Resto monto
+    //NO SE
+}
+
+// REQUERIMIENTO 7
+public float finalizarVenta(int numeroVenta, boolean confirmar)
+        throws VentaNoExisteException, VentaYaFinalizadaException {
+
+//Busco la venta
+Venta ventaEncontrada = null;
+int i = 0;
+while (i < listaVentas.size() && ventaEncontrada == null) {
+     Venta v = listaVentas.get(i);
+       if (v.getNumero() == numeroVenta) {
+            ventaEncontrada = v;
+        }
+
+        i++;
+    }
+if (ventaEncontrada == null) {
+        throw new VentaNoExisteException("El numero de venta no existe.");
+    }
+
+  // Verifico el estado si esta en prooceso o no
+ if (!ventaEncontrada.getEstado().equals("EN_PROCESO")) {
+      throw new VentaYaFinalizadaException("La venta ya fue pagada.");
+    }
+
+// Verifico si tiene postres, si cantTotal=0 es que no hay postres
+    Orden ordenVenta = ventaEncontrada.getOrden();
+
+    if (ordenVenta.calcularCantTotal() == 0) {
+
+ // No tiene postres elimino venta
+  listaVentas.remove(ventaEncontrada);
+        return 0;
+ }
+
+//Si confirma
+if (confirmar) {
+  ventaEncontrada.setEstado("FINALIZADA");
+      return ventaEncontrada.getMontoTotal();
+}
+else {
+// Si cancela, elimino venta
+    listaVentas.remove(ventaEncontrada);
+      return 0;
+    }
+}
+
+// REQUERIMIENTO 8
+public void listadoVentas(char letra) {
+
+  for (int i = 0; i < listaVentas.size(); i++) {
+       Venta v = listaVentas.get(i);
+ // Si la indicación es T → mostrar todas
+        if (letra == 'T') {
+System.out.println(
+                "Numero: " + v.getNumero() +
+                " Fecha: " + v.getFecha() +
+                " Direccion: " + v.getDireccionEntrega() +
+                " Monto: " + v.getMontoTotal() +
+                " Estado: " + v.getEstado()
+            );
+        }
+
+  // Si la indicación es P=solo en proceso
+        else if (letra == 'P') {
+             if (v.getEstado().equals("EN_PROCESO")) {
+
+                System.out.println(
+                    "Numero: " + v.getNumero() +
+                    " Fecha: " + v.getFecha() +
+                    " Direccion: " + v.getDireccionEntrega() +
+                    " Monto: " + v.getMontoTotal() +
+                    " Estado: " + v.getEstado()
+                );
+            }
+        }
+
+   // Si la indicación es F=solo finalizadas
+        else if (letra == 'F') {
+          if (v.getEstado().equals("FINALIZADA")) {
+              System.out.println(
+                    "Numero: " + v.getNumero() +
+                    " Fecha: " + v.getFecha() +
+                    " Direccion: " + v.getDireccionEntrega() +
+                    " Monto: " + v.getMontoTotal() +
+                    " Estado: " + v.getEstado()
+                );
+            }
+        }
+    }
+}
+
+// REQUERIMIENTO 9
+public void listadoPostresDeVenta(int numeroVenta)
+        throws VentaNoExisteException {
+
+    // Busco la venta
+    Venta ventaEncontrada = null;
+
+    for (int i = 0; i < listaVentas.size(); i++) {
+        Venta v = listaVentas.get(i);
+
+        if (v.getNumero() == numeroVenta) {
+            ventaEncontrada = v;
+            break;
+        }
+    }
+
+    //Si no existe = error
+    if (ventaEncontrada == null) {
+        throw new VentaNoExisteException("El número de venta no existe.");
+    }
+
+    //Obtener la orden
+    Orden ordenVenta = ventaEncontrada.getOrden();
+
+    //Recorro todos los postres de una orden y asi sucesivamente
+  for (int i = 0; i < ordenVenta.getTope(); i++) {
+      //cada item tiene postre y cant
+        ItemOrden item = ordenVenta.getItem(i);
+        Postre p = item.getPostre();
+
+System.out.println(
+                "Codigo: " + p.getCodigo() +
+                " Nombre: " + p.getNombre() +
+                " Precio: " + p.getPrecio() +
+                " Tipo: " + p.getTipoPostre() +
+                " Cantidad: " + item.getCantidad()
+        );
+    }
+}
